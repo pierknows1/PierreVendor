f42d5d913a20013bc0775778c4bfcb6c7716af03
pier <pierknows@gmail.com>
2023-07-24 08:40:51 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PierreVendor.Models;
+
+namespace PierreVendor.Controllers;
+
+public class HomeController : Controller
+{
+   
+
+}
---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace PierreVendor.Models
+{
+    public class Order
+   {
+
+   }
+}
